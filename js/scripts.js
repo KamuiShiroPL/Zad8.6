1ebd648b8b5ffa1090a55849e51123cdbdf1bd39
Pawel Dudek <paweldudek1988@gmail.com>
2018-01-12 18:58:45 +0900

    $ git commit -m

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Traingle area
+
+var a=prompt("Base", "Base?");
+var h=prompt("Heigh", "height?");
+var triangleArea = (a * h)/2;
+
+document.write("Area is ",  a * h / 2);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
